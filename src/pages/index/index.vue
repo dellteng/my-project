8c04cd6fce7bb1e9047fccc7e99e76f7d7060cee
dellteng <TengD_a@163.com>
2018-04-18 16:01:31 +0800

    'change'

---
+++ src/pages/index/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="header">header</div>
+</template>
+
+<script>
+export default {
+  name: 'index'
+}
+</script>
+
+<style scoped>
+  .header {
+    background: red;
+  }
+</style>
